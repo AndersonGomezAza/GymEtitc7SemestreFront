6d2e852bdaba042542c97b2e4f2515d6dd42c574
AndersonGomezAza <argomeza@itc.edu.co>
2023-09-22 12:03:39 -0500

    Uploading the info in table about maquinarias

--- src/app/components/maquinarias/maquinarias.component.ts
+++ src/app/components/maquinarias/maquinarias.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
@@ -7,11 +10,41 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./maquinarias.component.css']
 })
 export class MaquinariasComponent implements OnInit{
-  tituloMaquinaria = "Maquinarias";
+  displayedColumns: string[] = ['idMaquinaria', 'nombreMaquinaria', 'descripcionMaquinaria', 'categoriaMaquinaria', 'estadoMaquinaria', 'serialMaquinaria'];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor(public apiService: ApiService) {}
+  columnHeaders = {
+    idMaquinaria: 'Id Actividad',
+    nombreMaquinaria: 'Nombre',
+    descripcionMaquinaria: 'Descripcion',
+    categoriaMaquinaria: 'Categoria',
+    estadoMaquinaria: 'Estado',
+    serialMaquinaria: 'Serial',
+  };
+
+  constructor(public apiService: ApiService) {
+    this.dataSource = new MatTableDataSource()
+  }
 
   ngOnInit(): void {
-    this.apiService.Get("Maquinarias");
+    this.apiService.Get("Maquinarias").then((res)=>{
+      this.dataSource.data = res;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
